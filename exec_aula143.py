e6c47f10955a1da61c2c26a6e0b77c1c24c3aa59
HæLL <israelaraujo.emply@gmail.com>
2023-08-29 20:11:52 -0300

    Truthy e Falsy, tipos mutáveis e imutaveis.

---
+++ exec_aula143.py
@@ -0,0 +1,28 @@
+# Truthy e Falsy, tipos mutáveis e imutaveis.
+# Mutaveis [] {} set()
+# Imutáveis () "" 0 0.0 None False range(0, 10)
+lista = []
+dicionario = {}
+conjunto = set()
+tupla = ()
+string = ''
+inteiro = 0
+flutuante = 0.0
+nada = None
+falso = False
+intervalo = range(0)
+
+def falsy(valor):
+    return 'falsy' if not valor else 'truthy'
+
+print(f'TESTE', falsy('TESTE'))
+print(f'{lista=}', falsy(lista))
+print(f'{dicionario=}', falsy(dicionario))
+print(f'{conjunto=}', falsy(conjunto))
+print(f'{tupla=}', falsy(tupla))
+print(f'{string=}', falsy(string))
+print(f'{inteiro=}', falsy(inteiro))
+print(f'{flutuante=}', falsy(flutuante))
+print(f'{nada=}', falsy(nada))
+print(f'{falso=}', falsy(falso))
+print(f'{intervalo=}', falsy(intervalo))
