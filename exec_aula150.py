d231bf83cd79b84df0d3e1fc70dc3bd4f76d7c02
HæLL <israelaraujo.emply@gmail.com>
2023-09-14 20:17:50 -0300

    (Parte 2) try e except com else e finally

---
+++ exec_aula150.py
@@ -0,0 +1,17 @@
+# (Parte 1) try e except com else e finally
+
+# a = 18
+# b = 0
+# c = a / b
+
+try:
+    print('ABRIR ARQUIVO')
+    8/0
+except ZeroDivisionError as e:
+    print(f'{e.__class__.__name__}: {e}')
+except IndexError:
+    print('Erro de index')
+else:
+    print('Sem erros.')
+finally:
+    print('FECHAR ARQUIVO')
