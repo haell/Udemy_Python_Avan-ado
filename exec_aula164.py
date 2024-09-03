b738b9f3fd5efdba016c5dc4ea19764f47ff3fd2
HæLL <israelaraujo.emply@gmail.com>
2024-09-03 14:11:02 -0300

    Variáveis livres + nonlocal(locals, globals)

---
+++ exec_aula164.py
@@ -0,0 +1,17 @@
+# Variáveis livres + nonlocal(locals, globals)
+
+def concatenar(string_inicial):
+    valor_final = string_inicial
+
+    def interna(valor_a_concatenar=''):
+        nonlocal valor_final
+        valor_final += valor_a_concatenar
+        return valor_final
+    return interna
+
+c = concatenar('a')
+print(c('b'))
+print(c('c'))
+print(c('d'))
+final =  c()
+print(final)
